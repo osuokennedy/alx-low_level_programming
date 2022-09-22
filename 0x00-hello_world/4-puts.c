bf5371fd5a715eeca7327bd0a9c2ea9aa9c94f3f
osuokennedy <osuokennedy@gmail.com>
2022-09-22 05:02:46 -0700

    printing text in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *main - Entry point
+ *
+ *Return:  Always (success)
+ *
+ */
+int main (void)
+{
+  puts ("\" Programming is like buiding a multilingual puzzle");
+  return (0);
+}
