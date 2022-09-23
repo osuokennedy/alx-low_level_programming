0c04195dfba96ceb7dd09537d371314b57eb2ae5
osuokennedy <osuokennedy@gmail.com>
2022-09-23 05:23:24 -0700

    size of var types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ printf("Size of char: %zu bytes\n", sizeof(char));
 printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of long int: %zu bytes\n", sizeof(long int));
 printf("Size of long long int: %zu bytes\n", sizeof(long long int));
- printf("size of float: %zu bytes\n", sizeof(float));
+printf("size of float: %zu bytes\n", sizeof(float));
 return (0);
 
 }
