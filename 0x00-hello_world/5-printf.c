14bc9f133f9300f941f5e3ff7582fd8c3016624c
osuokennedy <osuokennedy@gmail.com>
2022-09-23 01:11:51 -0700

    updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
  *main - Entrry point
  *
  *return: Always 0 (success)
+ *
  */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
-return 0;
+ printf("\"with proper grammar, but the outcome is a piece of art,");
+ return 0;
 }
    
