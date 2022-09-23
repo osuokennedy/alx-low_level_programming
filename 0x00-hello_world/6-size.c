f75e52f48fa40b87af99e38272301e8cd9fcee45
osuokennedy <osuokennedy@gmail.com>
2022-09-23 05:10:18 -0700

    size of var

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,10 @@
  */
 int main(void)
 {
-int intType;
-float floatType;
-double doubleType;
-char charType;
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of char: %zu bytes\n", sizeof(charType));
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of long int: %zu bytes\n", sizeof(long int));
+printf("Size of long long int: %zu bytes\n", sizeof(long long int));
 
 return (0);
 
