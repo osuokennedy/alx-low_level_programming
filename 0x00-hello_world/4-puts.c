6b603cdebd2e3af96ed75227c85b0a3f52ae14f4
osuokennedy <osuokennedy@gmail.com>
2022-12-08 07:46:37 -0800

    using puts to print line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@
 
 
 int main(void)
-{	
-	puts("\" Programming is like buiding a multilingual puzzle");
-	return (0);
+{
+puts("\" Programming is like buiding a multilingual puzzle");
+return (0);
 }
