5c8dcec4d0c027701d896f367f4917c5bac6aa2f
osuokennedy <osuokennedy@gmail.com>
2022-12-12 11:30:09 -0800

    all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -  All possible combinations of single-digit numbers.
+ * Return:
+ *Return: Always 0
+ */
+int main(void)
+{
+	int Alpha;
+
+	for (Alpha = 48; Alpha < 58; Alpha++)
+	{
+	putchar(Alpha);
+	if (Alpha != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	return (0);
+}
