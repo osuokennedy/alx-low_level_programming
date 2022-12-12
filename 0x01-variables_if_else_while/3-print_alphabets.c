fcf3b232d74c5a22b034beed3f6e4b4865cb26c3
osuokennedy <osuokennedy@gmail.com>
2022-12-12 08:56:32 -0800

    numbers

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry           
+ * Return: Always 0 (Success)
+ */                                                                                                                                                                                 
+int main(void)                                                                                                                                                                      
+{
+	char woman;
+	char man
+	for(woman = 'a'; woman <= 'z'; woman++);
+	putchar('woman')
+	for(man = 'A'; man <= 'Z'; man++);
+	putchar('man');
+	putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Retuen: Always 0
+ */
+int main(void)
+{
+	int Alpha;
+
+	for (Alpha = 0; Alpha <= 10; Alpha++)
+		printf("%d", Alpha);
+	printf("\n");
+	return (0);
+}
