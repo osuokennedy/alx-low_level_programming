2bc84c1ca4fc011605714d1c8e8477376f91db2b
osuokennedy <osuokennedy@gmail.com>
2022-12-13 06:00:42 -0800

    Lower and upper alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 /**
- * main - Entry           
- * Return: Always 0 (Success)
- */                                                                                                                                                                                 
-int main(void)                                                                                                                                                                      
+ *main - Entry point
+ *
+ *Return: Always 0
+ *
+ */
+int main(void)
 {
-	char woman;
-	char man
-	for(woman = 'a'; woman <= 'z'; woman++);
-	putchar('woman')
-	for(man = 'A'; man <= 'Z'; man++);
-	putchar('man');
-	putchar('\n');
+char c;
+char d;
+
+c = 'a';
+d = 'A';
+while (c <= 'z')
+{
+putchar (c);
+c++;
+}
+while (d <= 'Z')
+{
+putchar (d);
+d++;
+}
+putchar ('\n');
 return (0);
 }
