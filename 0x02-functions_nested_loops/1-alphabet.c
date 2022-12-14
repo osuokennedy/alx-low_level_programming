dac4ab57172ef53ec4eac42302e6a9b239b352a0
osuokennedy <osuokennedy@gmail.com>
2022-12-14 06:51:55 -0800

    Lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet - Entry point
+ *
+ *Return: always 0
+ */
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar('c');
+c++;
+}
+_putchar('\n');
+return ('0');
+}
