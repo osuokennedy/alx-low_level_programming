ebb3337fd349f9f09ef0ce94bc6140c4e600ed32
osuokennedy <osuokennedy@gmail.com>
2022-12-16 13:36:31 +0300

    Uppercase cahracter

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+0x04. C - More functions, more nested loops
+0. isupper
+1. isdigit
+2. Collaboration is multiplication
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ *_putchar - writes the character c to std ouput
+ *
+ *@c: the character to print
+ *
+ *Return: Onsuccess 1.
+ *On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
