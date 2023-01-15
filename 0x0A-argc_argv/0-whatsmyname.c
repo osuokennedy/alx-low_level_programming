a1bd337d43a70007efab2cc7203b3690ee22affd
osuokennedy <osuokennedy@gmail.com>
2023-01-15 05:38:37 +0300

    Print my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - This is a function that prints its name
+ @argc: argc parameter
+ *@argv: An array o a command listed
+ *Return: 0 Always Success
+ */
+int main(int argc, int argv[0])
+{
+printf("%d\n", argc);
+return (0);
+}
